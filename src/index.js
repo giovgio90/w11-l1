ed956153253d599af449f8dd777adb2ad7572f9f
giovgio90 <giovgio90@gmail.com>
2023-09-19 17:29:39 +0200

    w11-l2 update 2.0 exercise completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
-)
+);
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,14 +1,32 @@
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import favouriteReducer from "../reducers/favouriteReducer";
 import searchResultsReducer from "../reducers/searchResultsReducer";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_PERSIST_KEY,
+    }),
+  ],
+};
 
 const rootReducer = combineReducers({
   favourite: favouriteReducer,
   searchResults: searchResultsReducer,
 });
 
-const store = configureStore({
-  reducer: rootReducer,
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
 
-export default store;
+export const persistor = persistStore(store);
